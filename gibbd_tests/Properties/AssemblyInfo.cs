9916a38b58e4030939d64ac5d556e72be099a089
Alesysss <129395303+Alesysss@users.noreply.github.com>
2024-12-02 11:44:51 +0300

    Add files via upload

---
+++ gibbd_tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("gibbd_tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gibbd_tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("766e8ba8-005d-45ca-8ab5-30de1c502e1f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ gibbd_tests/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿// В gibbd_tests/UnitTest1.cs
+
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YchebPrakGIBDD.Windows;
+
+namespace gibbd_tests
+{
+    [TestClass]
+    public class AvtorizaciaTests
+    {
+        // 1. Тестирование успешного входа
+        [TestMethod]
+        public void TestButtonAvtoriz_SuccessfulLogin()
+        {
+            var window = new Avtorizacia();
+            window.TBlogin.Text = "inspector001";
+            window.TBparol.Text = "inspector001";
+
+            MethodInfo method = typeof(Avtorizacia).GetMethod("ButtonAvtoriz", BindingFlags.NonPublic | BindingFlags.Instance);
+            method.Invoke(window, new object[] { null, null });
+
+            Assert.AreEqual("Вход в систему", window.LastMessage);
+        }
+
+        // 2. Тестирование пустых логина и пароля
+        [TestMethod]
+        public void TestButtonAvtoriz_EmptyLoginAndPassword()
+        {
+            var window = new Avtorizacia();
+            window.TBlogin.Text = "";
+            window.TBparol.Text = "";
+
+            MethodInfo method = typeof(Avtorizacia).GetMethod("ButtonAvtoriz", BindingFlags.NonPublic | BindingFlags.Instance);
+            method.Invoke(window, new object[] { null, null });
+
+            Assert.AreEqual("Заполните все поля", window.LastMessage);
+        }
+
+        // 3. Тестирование пустого пароля
+        [TestMethod]
+        public void TestButtonAvtoriz_EmptyPassword()
+        {
+            var window = new Avtorizacia();
+            window.TBlogin.Text = "inspector001";
+            window.TBparol.Text = "";
+
+            MethodInfo method = typeof(Avtorizacia).GetMethod("ButtonAvtoriz", BindingFlags.NonPublic | BindingFlags.Instance);
+            method.Invoke(window, new object[] { null, null });
+
+            Assert.AreEqual("Заполните все поля", window.LastMessage);
+        }
+
+        // 4. Тестирование пустого логина
+        [TestMethod]
+        public void TestButtonAvtoriz_EmptyLogin()
+        {
+            var window = new Avtorizacia();
+            window.TBlogin.Text = "";
+            window.TBparol.Text = "inspector001";
+
+            MethodInfo method = typeof(Avtorizacia).GetMethod("ButtonAvtoriz", BindingFlags.NonPublic | BindingFlags.Instance);
+            method.Invoke(window, new object[] { null, null });
+
+            Assert.AreEqual("Заполните все поля", window.LastMessage);
+        }
+    }
+}
